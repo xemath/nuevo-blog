5f62c29248e793f7c7d4f3b1a383da9a967ab882
xemath <carlosfernandocastroposada@gmail.com>
2021-05-27 13:31:30 -0500

    inicio blog estatico con 5 proyectos!

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,70 +1,23 @@
-import React from 'react'
-import fetch from 'isomorphic-fetch';
-
-
+import React from 'react';
+import "../fondo.jpg"
 class Post extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            post: [],
-            contento: [],
-            url: '',
-        }
-    }
-    componentDidMount(){
-        fetch(`http://localhost:1337/posts/${this.props.match.params.id}`)
-        .then((response)=>{
-        if(response.status >= 400){
-            throw new Error('algo malo ha pasado papa');
-        }
-        return response.json();
-        })
-        .then((post)=>{
-            let r = post.content.split('#');
-            console.log(post.image.url)
-            this.setState({post:post});
-            this.setState({contento:r});
-            this.setState({url:post.image.url})
-
-        })
-    }
-
-
-
+    
+    
     render(){  
     return (
         <div className="container">
-            {
-            console.log(this.state.url)
-            }
+            
             <div className="row">
                 <div className="col-sm-8 p-2 mx-auto">
                     <div className=" bg-white super-bordo">
                     <div className="img1">
-                                <img src={`http://localhost:1337${this.state.url}`} alt=""/>
+                                <img src="/images/fondo.jpg" alt="..."/>
                             </div>
                             
                             <div className="p-4 rounded mt-3">
-                                <h3 className="fw-bold text-left mb-3">{this.state.post.titulo}</h3>
-                                {
-                                    this.state.contento.map(e=>{
-                                        let x = e;
-                                        if(e.charAt(0)==='p'){
-                                            return <p className="mb-4">{x.slice(1)}</p>
-                                        }
-                                        else if(e.charAt(0)==='t'){
-                                            return <h5 className="mb-3 fw-bold">{x.slice(1)}</h5>
-                                        }
-                                        else if(e.charAt(0)==='i'){
-                                            //console.log(x.slice(1));
-                                            return <img src={`http://localhost:1337/${x.slice(1)}`} className ="img-fluid rounded mb-3" alt=""/>
-                                        }
-                                        else{
-                                            return <p>e</p>
-                                        }
-                                        
-                                    })
-                                }
+                                <h3 className="fw-bold text-left mb-3">"titulo"</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere incidunt animi doloribus fugit inventore illo natus voluptatibus consectetur adipisci. Molestiae.</p>
+                                
                             </div>
                     </div>
                                     
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,43 +1,51 @@
 import React from 'react';
 import ProyectoTarjeta from './ProyectoTarjeta';
-import fetch from 'isomorphic-fetch';
-class Projects extends React.Component{
-  
-  constructor(){
-    super();
-    this.state = {
-      proyectos:[]
-    }
-  }
 
-  componentDidMount(){
-    fetch('http://localhost:1337/proyectos/')
-    .then((response)=>{
-      if(response.status >= 400){
-        throw new Error('algo malo ha pasado papa');
-      }
-      return response.json();
-    })
-    .then((proyectos)=>{
-      this.setState({proyectos:proyectos})
-    })
-  }  
-  
+
+class Projects extends React.Component{
+
+  // titulo - descripcion urlImagen, linkProyecto, linkGit
   render(){
     return(
       <div className="container p-0" id="cuerpo-blogs">
         <h1 className="titulo display-1 text-center mt-5"> Proyectos </h1>
         <div className="row mt-5">
-          {
-            this.state.proyectos.map((proyecto)=>{
-              console.log(proyecto.linkProyecto)
-              return <ProyectoTarjeta titulo={`${proyecto.Titulo}`} descripcion={`${proyecto.descripcion}`} urlImagen={`http://localhost:1337${proyecto.img[0].url}`}
-              linkProyecto={`${proyecto.linkProyecto}`}
-              linkGit={`${proyecto.linkGit}`}
-              />
-            })
-          }
-          
+          <ProyectoTarjeta
+          titulo="Aplicacion de encuestas"
+          descripcion="Permite crear, guardar, ver y responder encuestas."
+          urlImagen="images/p1.png"
+          linkProyecto="https://encuestas-mongo.herokuapp.com/"
+          linkGit="https://github.com/xemath/app-encuestas"
+          />
+          <ProyectoTarjeta
+          titulo="Aplicacion de trivias"
+          descripcion="permite responder preguntas de tipo 'preguntados' y clasificar los usuarios por tiempo y puntaje"
+          urlImagen="images/p2.png"
+          linkProyecto="https://preguntas-mysql.herokuapp.com/"
+          linkGit="https://github.com/xemath/trivias-app"
+          />
+          <ProyectoTarjeta
+          titulo="Blog en React con Bootstrap"
+          descripcion="El blog que estas usando ahora mismo. hay una version con back end y contenido dinamico y otra vesion estatica"
+          urlImagen="images/p3.png"
+          linkProyecto="https://castro-blog.netlify.app/"
+          linkGit="https://github.com/xemath/nuevo-blog"
+          />
+          <ProyectoTarjeta
+          titulo="Practica HTML y CSS puro"
+          descripcion="Challenge de 'frontendmentor'"
+          urlImagen="images/p4.png"
+          linkProyecto="https://xemath.github.io/profile-card-component/"
+          linkGit="https://github.com/xemath/profile-card-component"
+          />
+          <ProyectoTarjeta
+          titulo="Practica de HTML y CSS puro"
+          descripcion="Challenge de 'frontendmentor'"
+          urlImagen="images/p5.png"
+          linkProyecto="https://xemath.github.io/3-card-preview-challenge/"
+          linkGit="https://github.com/xemath/3-card-preview-challenge"
+          />
+
         </div>
     </div>
     )
