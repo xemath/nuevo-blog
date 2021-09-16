9b94e409131eb5ba1e793bebfeb2f65e15588122
xemath <carlosfernandocastroposada@gmail.com>
2021-09-16 10:01:54 -0500

    añadi los proyectos de front

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,6 +10,34 @@ class Projects extends React.Component{
       <div className="container p-0" id="cuerpo-blogs">
         <h1 className="titulo display-1 text-center mt-5"> Proyectos </h1>
         <div className="row mt-5">
+        <ProyectoTarjeta
+          titulo="Panel de Administracion HTML, CSS y Vanilla Javascript"
+          descripcion="Simulacion de un panel de administracion con diseño responsivo"
+          urlImagen="images/n1.png"
+          linkProyecto="https://xemath.github.io/panel/"
+          linkGit="https://github.com/xemath/panel/"
+          />
+          <ProyectoTarjeta
+          titulo="Landing Page responsivo HTML, CSS, React"
+          descripcion="Landing page Responsivo con temporizador y slider en React"
+          urlImagen="images/n2.png"
+          linkProyecto="https://kfc77.netlify.app/"
+          linkGit="https://github.com/xemath/pagina-inicio-kfc"
+          />
+          <ProyectoTarjeta
+          titulo="Landing Page responsivo HTML, CSS, React"
+          descripcion="Landing page Responsivo con cambio de tema en React"
+          urlImagen="images/n3.png"
+          linkProyecto="https://starbucks77.netlify.app/"
+          linkGit="https://github.com/xemath/pagina-inicio-starbucks"
+          />
+          <ProyectoTarjeta
+          titulo="Pagina restaurante HTML, CSS, Vanilla Js"
+          descripcion="Pagina para restaurante responsiva"
+          urlImagen="images/n4.png"
+          linkProyecto="https://restaurante77.netlify.app/"
+          linkGit="https://github.com/xemath/pagina-restaurante"
+          />
           <ProyectoTarjeta
           titulo="Aplicacion de encuestas"
           descripcion="Permite crear, guardar, ver y responder encuestas."
@@ -31,20 +59,8 @@ class Projects extends React.Component{
           linkProyecto="https://castro-blog.netlify.app/"
           linkGit="https://github.com/xemath/nuevo-blog"
           />
-          <ProyectoTarjeta
-          titulo="Practica HTML y CSS puro"
-          descripcion="Challenge de 'frontendmentor'"
-          urlImagen="images/p4.png"
-          linkProyecto="https://xemath.github.io/profile-card-component/"
-          linkGit="https://github.com/xemath/profile-card-component"
-          />
-          <ProyectoTarjeta
-          titulo="Practica de HTML y CSS puro"
-          descripcion="Challenge de 'frontendmentor'"
-          urlImagen="images/p5.png"
-          linkProyecto="https://xemath.github.io/3-card-preview-challenge/"
-          linkGit="https://github.com/xemath/3-card-preview-challenge"
-          />
+          
+          
 
         </div>
     </div>
